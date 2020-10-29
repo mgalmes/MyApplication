0ff1ec1b2115683bac733862975eb09330cc5892
Marc Galmes <marcgalmes2a@outlook.com>
2020-10-29 13:46:48 +0100

    Task 1. Create a new "Hello Toast" project

--- app/src/main/java/com/github/mgalmes/myapplication/MainActivity.java
+++ app/src/main/java/com/github/mgalmes/myapplication/MainActivity.java
@@ -18,33 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "Hello World");
     }
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-        System.out.println(event);
-
-        int action = event.getAction();
-
-        switch (action) {
-            case (MotionEvent.ACTION_DOWN):
-                toast("Action was DOWN");
-                return true;
-            case (MotionEvent.ACTION_MOVE):
-                toast("Action was MOVE");
-                return true;
-            case (MotionEvent.ACTION_UP):
-                toast("Action was UP");
-                return true;
-            case (MotionEvent.ACTION_CANCEL):
-                toast("Action was CANCEL");
-                return true;
-            case (MotionEvent.ACTION_OUTSIDE):
-                toast("Movement occurred outside bounds " +
-                        "of current screen element");
-                return true;
-            default:
-                return super.onTouchEvent(event);
-        }
-    }
 
     private void toast(String text) {
         Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
